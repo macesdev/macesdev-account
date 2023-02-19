1cdadaf38ae36d1ccc01e897bedc3e38b7ad76b3
mamiiblt <macesgamesstudios@gmail.com>
2023-02-19 03:25:05 +0300

    mdev.mdkn@latest - 02-19-2023, 03:25:04

---
+++ README.md
@@ -0,0 +1,6 @@
+# macesdev account
+
+Bu proje MDKN ile otomatik olarak derlenip yayınlanmaktadır, sayfanın kaynaığını görmek için
+gh-pages dalına bakabilirsiniz.
+
+selamlaaar!
---
+++ build.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+now=$(date +"%m-%d-%Y, "%T"")
+
+echo " "
+echo "------------- macesdev foundation -------------"
+echo "---------------- mdkn@23.1.4.7 ---------------- "
+echo " "
+
+echo "◉ - Dosyalar güncelleniyor.."
+git add . 
+git add -A &>/dev/null
+echo "✔ - Dosyalar güncellendi!"
+echo "◉ - Uzak değişiklikler ekleniyor.."
+git fetch &>/dev/null
+echo "✔ - Değişiklikler eklendi!"
+echo "◉ - Dallar güncelleniyor.."
+git merge &>/dev/null
+echo "✔ - Dallar güncellendi!"
+echo "◉ - Değişiklikler temin ediliyor.."
+git commit -m "mdev.mdkn@latest - $now" &>/dev/null
+echo "✔ - Değişilikler eklendi!"
+echo "◉ - Güncelleniyor.."
+git push -f origin main &>/dev/null
+echo "✔ - Güncellendi!"
+
+
+echo " "
+echo "Güncelleme başarılı, mdev.account.build çalıştırılmak üzere sevk ediliyor.."
+echo " "
